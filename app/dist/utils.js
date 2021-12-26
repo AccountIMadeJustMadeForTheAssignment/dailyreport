b63f09da8c00518a017a977a3517b4eb96d5c7a3
Fabian Sellmann <=>
2021-12-26 21:26:33 +0800

    remove dist

--- app/dist/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateToUTCDayUnixTimestamp = void 0;
-const dateToUTCDayUnixTimestamp = (_date) => {
-    const date = new Date(Date.UTC(_date.getUTCFullYear(), _date.getUTCMonth(), _date.getUTCDay(), 0, 0, 0));
-    return date.getTime() / 1000;
-};
-exports.dateToUTCDayUnixTimestamp = dateToUTCDayUnixTimestamp;
